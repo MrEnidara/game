dc59d94f706b39ade5cfd1796a051dbecf715d8e
MrEnidara <91989742+MrEnidara@users.noreply.github.com>
2025-05-19 12:02:25 +0300

    1

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,22 +2,34 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    bg_color = (230, 230, 230)
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    alien = Alien(ai_settings, screen)
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
